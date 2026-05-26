92d8f72749835e8eb787ec46b9f72feb259a6ca6
Vitor De Araujo Ferreira <vitoraraujoferreira2005@gmail.com>
2026-05-25 21:43:06 -0300

    implementação do arquivo decision.py

--- decison.py
+++ decison.py
@@ -1,4 +1,21 @@
 ###toma as decisões da nave
-from data import sistemas 
+from energy import calcular_energia_total, calcular_consumo_total
 
 #vamos pensar para desenvolver isso. 
+
+def tomar_decisoes():
+    energia = calcular_energia_total()
+    consumo = calcular_consumo_total()
+    
+
+#regras 
+
+    if energia < 50 and consumo > 70:
+        print("ALERTA!: Ativar modo de economia.")
+       
+    elif consumo > energia:
+        print("ATENÇÃO!: Reduza levemente o consumo de energia.")
+ 
+    else:
+        print("Sistema energético estável.")
+
