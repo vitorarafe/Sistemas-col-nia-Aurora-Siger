ce22720db6fd127fd204d4275b664d282c4673a5
Vitor De Araujo Ferreira <vitoraraujoferreira2005@gmail.com>
2026-05-25 22:46:16 -0300

    tratando conflitos

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ SGEC - Sistema de Gerenciamento Energético da Colônia
 from decison import tomar_decisoes
 from energy import analisar_energia 
 from structure import buscar_maior_consumo, ordem_prioridade
-import forecast.py  #
+from forecast import prever_energia_futura
 
 
 #criar excecução com base nas funções que serão criadas nos outro arquivos :) => amanhã organizo melhor 
 
- analisar_energia()
+analisar_energia()
 
 
 tomar_decisoes()
@@ -24,5 +24,6 @@ ordenados = ordem_prioridade()
 
 for sistema in ordenados:
     print(f"{sistema['nome']}, Prioridade: {sistema['prioridade']}")
-    
-      
+
+
+prever_energia_futura()
